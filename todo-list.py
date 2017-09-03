9e7b0f2e4e734be58f3d607f5ced2011b81bdfbf
Andreas <andreas@hagmann.name>
2017-09-03 14:41:23 +0200

    code cleanup

--- todo-list.py
+++ todo-list.py
@@ -1,22 +1,22 @@
 #!env python3
 
-# 
+#
 # todo-list
 # Copyright (C) 2017 https://github.com/ahagmann
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
-# 
+#
 
 import sys
 from PyQt4 import QtCore, QtGui, uic
@@ -282,6 +282,6 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     gui = MainWindow(args)
-    
+
     gui.show()
     sys.exit(app.exec_())
